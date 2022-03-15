f092d831986b3a6a5b7aaa58c1f3f67edbfc9547
zcaldwell <mintyst.john@gmail.com>
2022-03-15 16:51:06 -0700

    ontroler and test set up

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const dog = require('../lib/models/Dog');
 
 describe('any-api routes', () => {
   beforeEach(() => {
@@ -10,5 +11,17 @@ describe('any-api routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('should be able to create a dog', async () => {
+    const expected = {
+      name: 'Watson',
+      age: 7,
+    };
+    const res = await (
+      await request(app).post('/api/v1/dogs')
+    ).setEncoding(expected);
+
+    expect(res.body).toEqual({ id: expect.any(String), ...expected });
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS dogs;
+
+CREATE TABLE dogs (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INT NOT NULL CHECK (age > -1),
+    color TEXT
+);
