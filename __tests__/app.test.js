c10ee26938d48a403dafd5f6a643ee4e97792e79
zcaldwell <mintyst.john@gmail.com>
2022-03-16 16:36:27 -0700

    findAll written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const dog = require('../lib/models/Dog');
+const Dog = require('../lib/models/Dog');
 
 describe('any-api routes', () => {
   beforeEach(() => {
@@ -18,10 +18,15 @@ describe('any-api routes', () => {
       name: 'Watson',
       age: 7,
     };
-    const res = await (
-      await request(app).post('/api/v1/dogs')
-    ).setEncoding(expected);
+    const res = await request(app).post('/api/v1/dogs').send(expected);
 
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('get all the dogs', async () => {
+    const expected = await Dog.findAll();
+    const res = await request(app).get('/api/v1/dogs');
+
+    expect(res.body).toEqual(expected);
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -6,5 +6,4 @@ CREATE TABLE dogs (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name TEXT NOT NULL,
     age INT NOT NULL CHECK (age > -1),
-    color TEXT
 );
